2139c4ae307508c4ed8acea1d26939885a351e3f
Bradley Boutcher <bradleyboutcher@gmail.com>
2018-02-04 15:10:31 -0500

    Implemented mapbox on homepage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MapBox from './MapBox';
 
 class App extends Component {
+
+    constructor(props) {
+      super(props)
+      this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+
+    tick() {
+      this.setState({
+        date: new Date()
+      });
+    }
+    
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title"> {this.state.date.toLocaleDateString()} </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          It is {this.state.date.toLocaleTimeString()}.
         </p>
-      </div>
+    </div>,
+    <MapBox />
     );
   }
 }
---
+++ src/MapBox.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import mapboxgl from 'mapbox-gl'
+
+class Map extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            lng: -71.0580,
+            lat: 42.3604,
+            zoom: 13,
+        }
+    }
+
+    componentDidMount() {
+      mapboxgl.accessToken = 'pk.eyJ1IjoiYnJhZGxleWJvdXRjaGVyIiwiYSI6ImNqZDF3eHo4MTBsbm8ycXBhY3ZtdzZwNWMifQ.2u-Wk2dbshYXVO8qBk7G5Q';
+      this.map = new mapboxgl.Map({
+        container: this.mapContainer,
+        style: 'mapbox://styles/mapbox/streets-v9',
+        center: [this.state.lng, this.state.lat],
+        zoom: this.state.zoom,
+        attributionControl: false
+      });
+      this.map.addControl(new mapboxgl.AttributionControl(), 'top-left');
+    }
+  
+    componentWillUnmount() {
+      this.map.remove();
+    }
+  
+    render() {
+      const style = {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        width: '100%',
+        height: '100%'
+      };
+  
+      return <div style={style} ref={el => this.mapContainer = el} />;
+    }
+  }
+  
+export default Map  
