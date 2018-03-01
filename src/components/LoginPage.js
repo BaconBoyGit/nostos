0d5ec165bbf9a0a54aebfe9061b3c344f0ee9a2d
cfrandsen09 <34043600+cfrandsen09@users.noreply.github.com>
2018-03-01 12:20:22 -0500

    Add files via upload
    Updated pages

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Home from './home.js';
 import './LoginPage.css';
+import './applicationLink.js'
+import Application from '../components/Application';
+import Header from '../components/Header';
 
 var blight = require('../images/b-dark.svg');
 var blogo = require('../images/logo.svg');
@@ -8,45 +11,54 @@ var bseal = require('../images/seal.svg');
 
 
 //This class is designed to create the initial login page so people can either check the status of their permit or apply for a permit
+
+
 class login extends React.Component {
+
+
+   
+
     render () {   
-        return (            
-        <div className ="App">   
-        <div class="mn">
-             <header class="h" >
-                <div class="lo">
-                 <h2><img src= { blogo } alt="City of Boston" class="lo-i" /></h2>
-                </div> 
-                <div class="s">
-                     <img src= { bseal }  alt="City of Boston" class="s-i" />
-                </div>
-            </header>
-        </div>
+
+     
+        function toApplication()
+        {
+        
+            alert('Hello world');
+            
+        }   
+
+        return (      
+ 
+        <div className ="login">   
+        
+        <header>
+        <script type="text/javascript" src="./applicationLink.js/">
+        </script>
+        </header>
+
         <center>            
         <div class="b b-- b--fw">
              <div class="b-c">
-                <div class="sh ">
+              
                      <h2 class="sh-title">Application for Moving Truck Permit</h2>
-                </div>
+               
             </div>
         </div>
         </center>
-        <form action="https://boston.gov" method="GET">
+        <form action="https://boston.gov" method="POST">
                     <div class="fs">
-                    <div class="fs-c fs-c--i">
-                        <div class  ="txt">
-                        <div align="center">
-                            <label for="text" class="txt-l">Email</label>
-                
-                            <input type="email" placeholder="Email address" class="txt-f" /> 
-                        </div>
-                        </div>
-                        </div>
-                    <div class="fs-c fs-c--i">    
+                    <div align="center">
+                    <div class="txt g--6">
+                        <label for="first_name" class="txt-l txt-l--mt000">Email Address</label>
+                         <input id="first_name" type="text" placeholder="Email" class="txt-f" required/>
+                     </div>
+                     </div>
+                     <div class="fs-c fs-c--i">    
                         <div class="txt">
                         <div align="center">
                             <label for="text" class="txt-l">Password</label>
-                            <input id="text" type="password" placeholder="Password" class="txt-f" />
+                            <input id="text" type="password" placeholder="Password" class="txt-f" required />
                          </div>
                          </div>
                     </div>
@@ -56,19 +68,24 @@ class login extends React.Component {
                         <button type="submit" class="btn btn--700">Login</button>       
                     </center>
                     </div>
-                </div>
-            </form>  
-            <form action="https://www.boston.gov/how-reserve-parking-spot-your-moving-truck" method="GET">  
+                    </div>
+                     </form>  
+            
             <div class="bc bc--r">
                     <center>
-                        <button type="submit" class="btn btn--700">Apply</button>       
+        <button id="apply" class="btn btn--700" onClick={toApplication()} >Apply</button> 
+                    
                     </center>
                     </div>
-             </form>       
+                 
         </div>
-      
         )
     }
+
+    
+
+
+
 
     
 }
---
+++ src/components/applicationLink.js
@@ -0,0 +1,11 @@
+
+
+function toApplication()
+{
+
+    alert('Hello world');
+    
+}
+
+
+
