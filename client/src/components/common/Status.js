783ded5141f88a06ed552c12dd758f0e81deb2f7
Christine <cfrandsen09@gmail.com>
2018-05-04 19:40:08 -0400

    Display Permit Data
    Displays permit location and status of permit

--- client/src/components/common/Status.js
+++ client/src/components/common/Status.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types'; 
-import { fetchCompany } from '../../actions/actions';
+import nl2br from 'react-newline-to-break';
+
 
 /*
    The status component
@@ -12,29 +13,14 @@ import { fetchCompany } from '../../actions/actions';
 
 class Status extends React.Component {
   
-   /* componentWillMount() {
-        const { fetchCompany } = this.props
-        fetchCompany()
-    }*/
     // Render inline style attributes
-    
-
 
     render() {
 
-        const { isAuthenticated, user, permit } = this.props
-        //Currently static, will connect to database to be dynamic later
-       
-        var appNumber = ["1", "2", "3"];
-        var status = ["Accepted", "Pending", "Denied"];
-        var disStatus = '';
-        var i;
-        var address = ["1144 Aztec Road", "255 Grapevine Road", "1225 Santa Claus Lane"];
-        var test = permit;
+        const { user, permit } = this.props
 
-        if (permit)
-            if (permit.companies)
-                console.log(permit.companies.length)
+        var test = permit;
+        var disStatus = ''
 
         // container for our profile element
         const statusContainer = {
@@ -57,14 +43,20 @@ class Status extends React.Component {
         {
             if(permit.companies)
             {
-                for(i = 0; i<permit.companies.length; i++)
-                {
-                     x+= permit.companies[i].location
-                   // console.log(x)
-                }
+                     
+                    for (var i = 0; i < permit.companies.length; i++)
+                    {   
+                        var m = i+1;
+                        if(permit.companies[i].isPending)
+                        disStatus += "Application " + m + ": " + permit.companies[i]. location +", Status: Pending" + "\n";
+                        else if(permit.companies[i].isApproved)
+                        disStatus += "Application " + m + ": " + permit.companies[i]. location +", Status: Approved" + "\n";
+                        else
+                        disStatus += "Application " + m + ": " + permit.companies[i]. location +", Status: Denied" + "\n";
+                        
+                    }
             }
         }
-
 
         var test = 
         
@@ -77,30 +69,17 @@ class Status extends React.Component {
                 {permit && permit.companies && permit.companies[0].date} </td> 
                 
                 <td data-label='Address'>  {permit && permit.companies && permit.companies[0].location}  </td> 
-                    <td data-label='Date Applied'> {status[0]}  </td> 
                 </tr>
                 <tr> 
                     <td data-label='Date Applied'> {permit && permit.companies && permit.companies[1].date} </td> 
                     <td data-label='Address'>  {permit && permit.companies && permit.companies[1].location}  </td> 
-                    <td data-label='Date Applied'> {status[1]}  </td> 
                 </tr>
                 <tr> 
                     <td data-label='Date Applied'> {permit && permit.companies && permit.companies[2].date} </td> 
                     <td data-label='Address'>  {permit && permit.companies && permit.companies[2].location}  </td> 
-                    <td data-label='Date Applied'> {status[2]}  </td> 
                 </tr>
                 
             </tbody>
-
-        for (i = 0; i < appNumber.length; i++)
-        {
-            disStatus +=<tr> <td data-label='Date Applied'> {appNumber[i]} </td> 
-            <td data-label='Address'>  {address[i]}  </td> 
-                <td data-label='Date Applied'> {status[i]}  </td> 
-            </tr>;
-
-        }
-
        
       return (
           <div className = 'statusContainer' style = { statusContainer }>              
@@ -110,28 +89,39 @@ class Status extends React.Component {
                             <div class="h2 tt-u ta-c p-h300">{user.first} {user.last}'s Application Status</div>
                             <hr class="hr hr--sq m-h300 m-v500" />
                           
-                            <table border="1" cellPadding="1" cellSpacing="1" class="responsive-table responsive-table--horizontal">
+                          {/*  <table border="1" cellPadding="1" cellSpacing="1" class="responsive-table responsive-table--horizontal">
                                 <thead>
                                     <tr>
                                         <th>Date Applied For</th>
+                                        <th>Start Time</th>
+                                        <th>End Time</th>
                                         <th>Address</th>
                                         <th>Status</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                <td data-label='Address'>{x}</td>
+                                <td data-label='Address'>Hello</td>
+                                <td data-label='Address'>Howdy</td>
                                 </tbody>
-                            </table>
+                                </table>*/}
+
+                                    <div class="t--intro">
+                                    <div>
+                                        {nl2br(disStatus)}
+                                      </div>
+                                    </div>
                         </div>
                     </div>
-                </div>
+                </div> 
             </div>   
+
+       
         ) 
     }
+    
 }
 
 Status.propTypes = {
-    fetchCompany : PropTypes.func,
     permit : PropTypes.object
 }
 
